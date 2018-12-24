f18706f125ffc2aa5be24e796f968991324ce804
Pierre-Luc Bruyere <proteama@yahoo.com>
2018-12-24 10:27:43 -0500

    Error boundary

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+  constructor() {
+    super()
+    this.state = {
+      hasError: false
+    }
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true })
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Ooops. Not good</h1>
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
+import ErrorBoundary from '../components/ErrorBoundary'
 import './App.css';
 
 class App extends Component {
@@ -30,7 +31,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange = {this.onSearchChange} />
         <Scroll>
+        <ErrorBoundary>
           <CardList robots = {filteredRobots} />
+        </ErrorBoundary>
         </Scroll>
       </div>
   }
